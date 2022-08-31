e8e163843e1484d89b3059e0f96996a196b70f43
Archana Agrawal <archana.agrawal3@outlook.com>
2022-08-31 14:47:57 -0400

    add testing for adding item to the cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -3,8 +3,8 @@ describe('product_details', () => {
     cy.visit('http://localhost:3000')
   })
   it("clicks on add to cart and increases the number of items in the cart", () => {
-    cy.contains("My Cart (0)")
+    cy.contains("My Cart (0)");
     cy.contains("Add").first().click({force: true});
-    cy.contains("My Cart (1)")
+    cy.contains("My Cart (1)");
   }); 
 })
